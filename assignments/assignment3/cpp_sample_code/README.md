bc8f4179d35cf23134295e8399ac6b7b754d5b16
xinjin <jxinpku@gmail.com>
2019-02-04 14:16:30 -0500

    update lec03

--- assignments/assignment3/cpp_sample_code/README.md
+++
@@ -1,3 +0,0 @@
-This is the sample code for assignment2 written by Mingjian Liu.
-Mingjian's code works well and is clear.
-You can do assignment3 based on Mingjian's code.
--- assignments/assignment4/env.sh
+++
@@ -1,8 +0,0 @@
-THIS_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-
-# ---------------- EDIT THIS ------------------
-BMV2_PATH=$THIS_DIR/../bmv2
-# e.g. BMV2_PATH=$THIS_DIR/../bmv2
-P4C_BM_PATH=$THIS_DIR/../p4c-bmv2
-# e.g P4C_BM_PATH=$THIS_DIR/../p4c-bm
-# ---------------- END ------------------
